4955f6ad9c62997ff683fcf0299fdd148829cfec
Vide0Master <signatov2134@gmail.com>
2025-04-28 16:07:53 +0300

    test

--- bot.js
+++ bot.js
@@ -36,6 +36,10 @@ bot.command('clear', (ctx) => {
     }
 });
 
+bot.command('test',(ctx)=>{
+    ctx.reply('test')
+})
+
 function saveUserMessage(ctx) {
     const userId = ctx.from.id;
     if (!userMessages[userId]) {
